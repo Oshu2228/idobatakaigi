a6314280a50e12093265f996ad06570a6c95ebb9
Shunsuke Ono <ono0112sf@gmail.com>
2022-01-10 19:13:54 +0900

    apply-grid-layout

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,49 +1,11 @@
-import React, { useRef, useState } from 'react';
-import { Avatar, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react'
 
-import { gravatarPath } from '../gravatar';
-import MessageField from './MessageField';
-import MessageSubmitButton from './MessageSubmitButton';
-
-const useStyles = makeStyles({
-  root: {
-    gridRow: 2,
-    margin: '26px',
-  },
-});
-
-const MessageInputField = ({ name }) => {
-  const inputEl = useRef(null);
-  const [text, setText] = useState('');
-  const classes = useStyles();
-  const avatarPath = gravatarPath(name);
-
+const MessageInputField = () => {
   return (
-    <div className={classes.root}>
-      <Grid container>
-        <Grid item xs={1}>
-          <Avatar src={avatarPath} />
-        </Grid>
-        <Grid item xs={10}>
-          <MessageField
-            inputEl={inputEl}
-            name={name}
-            setText={setText}
-            text={text}
-          />
-        </Grid>
-        <Grid item xs={1}>
-          <MessageSubmitButton
-            inputEl={inputEl}
-            name={name}
-            setText={setText}
-            text={text}
-          />
-        </Grid>
-      </Grid>
+    <div>
+     Message Input Field
     </div>
-  );
-};
+  )
+}
 
-export default MessageInputField;
+export default MessageInputField
