f69332468ce8c1d867fba006779b616eb9cd51c9
Shunsuke Ono <ono0112sf@gmail.com>
2022-01-10 16:30:30 +0900

    create signinPage

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
-
-import Main from './Main';
+import React from 'react';
 import SignIn from './SignIn';
-import config from '../config.json';
+
 
 export default () => {
-  const [name, setName] = useState('');
-
-  if (config.signInEnabled && name === '') {
-    return <SignIn setName={setName} />;
-  } else {
-    return <Main name={name} />;
-  }
+  return(
+    <SignIn />
+  )
+  
 };
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -12,14 +17,11 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link
-        color="inherit"
-        href="https://twitter.com/diveintohacking"
-        target="_blank"
-        rel="noopener"
-      >
-        はむさん
-      </Link>
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
     </Typography>
   );
 }
@@ -44,23 +46,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn({ setName }) {
+export default function SignIn() {
   const classes = useStyles();
-  const [disabled, setDisabled] = useState(true);
-  const [string, setString] = useState('');
-  const [isComposed, setIsComposed] = useState(false);
-
-  useEffect(() => {
-    const disabled = string === '';
-    setDisabled(disabled);
-  }, [string]);
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
         <Typography component="h1" variant="h5">
-          ようこそ
+          Sign in
         </Typography>
         <form className={classes.form} noValidate>
           <TextField
@@ -68,35 +65,48 @@ export default function SignIn({ setName }) {
             margin="normal"
             required
             fullWidth
-            id="name"
-            label="ニックネーム"
-            name="name"
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
             autoFocus
-            onChange={(e) => setString(e.target.value)}
-            onKeyDown={(e) => {
-              if (isComposed) return;
-
-              if (e.key === 'Enter') {
-                setName(e.target.value);
-                e.preventDefault();
-              }
-            }}
-            onCompositionStart={() => setIsComposed(true)}
-            onCompositionEnd={() => setIsComposed(false)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
           />
           <Button
-            type="button"
+            type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
-            disabled={disabled}
-            onClick={() => {
-              setName(string);
-            }}
           >
-            はじめる
+            Sign In
           </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
         </form>
       </div>
       <Box mt={8}>
