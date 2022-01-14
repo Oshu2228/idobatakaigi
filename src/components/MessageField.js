a9c661a4f3d7e6fe7c9c561d3bf6b9d23802bac1
Shunsuke Ono <ono0112sf@gmail.com>
2022-01-14 16:25:22 +0900

    display new message

--- src/components/MessageField.js
+++ src/components/MessageField.js
@@ -17,7 +17,7 @@ const MessageField = ({inputEl,name,setText,text}) => {
         const text = e.target.value
         if(text === "") return
         if(e.key === "Enter"){
-          pushMessage({name: "おにょ",text})
+          pushMessage({name: name,text})
           setText("")
           e.preventDefault()
         }
